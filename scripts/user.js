e852bf95d36c4fa434ad8715a60d86762f6fa731
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-10-20 13:05:16 -0300

    Adiciona aside lateral, barra de fundo, gráfico de tarefas e estatística

--- scripts/user.js
+++ scripts/user.js
@@ -1,12 +1,18 @@
 const names = document.querySelectorAll(".name");
 const usernames = document.querySelectorAll(".username");
 
+// const tester =document.getElementById("tester");
+
+const tasksLength = document.querySelectorAll(".task-length");
+const tasksPending = document.querySelectorAll(".task-length-pending");
+const tasksDone = document.querySelectorAll(".task-length-done");
+
+const taskcontainer = document.getElementById("task-container");
+
 const fetchUserData = async () => {
 
     const username = sessionStorage.getItem("username");
     const token = sessionStorage.getItem("auth");
-    console.log(username, token);
-
 
     const response = await fetch(`https://backend-jsdinamico.vercel.app/user/${username}`, {
         method: 'GET',
@@ -19,11 +25,38 @@ const fetchUserData = async () => {
     })
 
     const userData = await response.json();
+    sessionStorage.setItem('userdata', JSON.stringify(userData));
+
     return userData;
 }
 
-const userdata = await fetchUserData();
-const user = userdata[0];
 
-names.forEach((el) => el.innerText = user.name);
-usernames.forEach((el) => el.innerText = user.username);
+// const userdata = await fetchUserData();
+// const user = userdata[0];
+
+let completed = 3;
+let pendent = 2;
+let tasklength = completed + pendent;
+
+
+names.forEach((el) => el.innerText = 'Placeholder da silva');
+usernames.forEach((el) => el.innerText = 'placehold');
+tasksLength.forEach((el) => el.innerText = tasklength);
+tasksPending.forEach((el) => el.innerText = pendent);
+tasksDone.forEach((el) => el.innerText = completed);
+
+const data = [{
+        values: [completed, pendent],
+        labels: ['Completas', 'Pendentes'],
+        textposition: 'inside',
+        type: 'pie', hole: 0.4,
+        marker: {
+            colors: ['#5ADD8E', '#435343']
+        }
+      }];
+
+Plotly.newPlot('graph', data, {paper_bgcolor: 'transparent', showlegend: false}, {displayModeBar: false})
+
+taskcontainer.onclick = () => {
+    taskcontainer.classList.toggle('open');
+}
